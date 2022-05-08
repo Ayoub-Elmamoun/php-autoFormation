7e57c4348f6e81f9666722b9b89e91ed46237ae0
Ayoub-Elmamoun <saadcrax@protonmail.com>
2022-05-08 23:29:16 +0100

    added

---
+++ var_dump.php
@@ -0,0 +1,19 @@
+
+
+<?php
+
+// dump the info about variable 
+
+$balance = 100;
+$message = 'Insufficient balance';
+
+var_dump($balance);
+var_dump($message);
+
+/* output
+
+int(100) string(20) "Insufficient balance"
+
+*/
+?>
+
