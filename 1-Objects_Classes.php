253a2effb11b4c6df583f1f9de2ba28ad47762e7
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-05-09 13:45:27 +0100

    added a classe

--- 1-Objects_Classes.php
+++ 1-Objects_Classes.php
@@ -1,10 +1,13 @@
 <?php 
  //playing with classes
+ 
  class student {
     public $name;
     public $age;
 
  }
+    // create new anstance of the class
+    // set value for name and age
 
     $new_student = new student();
     $new_student->name = "John";
