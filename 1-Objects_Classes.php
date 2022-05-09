77f0eca7c46e58be2eaf210a964c013e86f67c00
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-05-09 13:41:04 +0100

    added

---
+++ 1-Objects_Classes.php
@@ -0,0 +1,14 @@
+<?php 
+ //playing with classes
+ class student {
+    public $name;
+    public $age;
+
+ }
+
+    $new_student = new student();
+    $new_student->name = "John";
+    $new_student->age = "20";
+
+
+?>
