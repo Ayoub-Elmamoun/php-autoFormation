715ab94f9962b9a9640c9d6f872f45101a7cd7ca
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-05-10 13:27:46 +0100

    public and private done

--- 3-public_private.php
+++ 3-public_private.php
@@ -10,6 +10,10 @@ class public_Customer
     }
 }
 
+
+
+
+
 $customer = new public_Customer();
 $customer->name = 'Bob';
 echo $customer->getName(); // Bob
@@ -29,4 +33,27 @@ class private_Customer
 $private_customer = new private_Customer();
 $private_customer->name = 'Bob';
 echo $private_customer->getName(); // error
+
+
+
+
+class private_Customer2{
+    private $name;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
+}
+
+$private_customer2 = new private_Customer2();
+$private_customer2->setName('Bob_marley');
+echo $private_customer2->getName(); // Bob
+
 ?>
