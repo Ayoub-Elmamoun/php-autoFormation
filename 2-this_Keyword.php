fafabb3b626d874ba0bf84e1164f7c8ea1df3abd
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-05-10 12:58:39 +0100

    this finished

---
+++ 2-this_Keyword.php
@@ -0,0 +1,22 @@
+<?php
+
+class fruit
+{
+    public $name;
+    public $color;
+
+     function set_name($name){
+        $this->name = $name;
+     }
+        
+     function get_name(){
+        return $this->name;
+     }
+
+}
+
+    $new_fruit = new fruit();
+    $new_fruit->set_name("Apple");
+    
+    echo $new_fruit->get_name();
+
